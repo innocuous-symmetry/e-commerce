6563140629bee2c6af23f5b5e6bc22d45e30b376
Mikayla Dobson <93477693+innocuous-symmetry@users.noreply.github.com>
2022-10-07 17:42:23 -0500

    started a new branch with only the new front end directory

--- _client/src/util/Page.tsx
+++
@@ -1,9 +0,0 @@
-function Page({ children, classes }: any) {
-    return (
-        <section className={`page ${classes}`}>
-            {children}
-        </section>
-    )
-}
-
-export default Page;
